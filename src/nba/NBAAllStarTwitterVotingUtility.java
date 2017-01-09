8e4d9a9f4e6134d3a1367f40bd27752f133bf47e
Kristian Sooklal <kristiansooklal@gmail.com>
2017-01-09 14:32:44 -0500

    updated Twitter Voting Utility

--- src/nba/NBAAllStarTwitterVotingUtility.java
+++ src/nba/NBAAllStarTwitterVotingUtility.java
@@ -10,31 +10,32 @@ public final class NBAAllStarTwitterVotingUtility {
 					"Trey Burke", "Jason Smith", "Tomas Satoransky"};
 	public static final String [] WESTERN_CONFERENCE_PLAYERS = new String []
 			{"Trevor Ariza", "Zaza Pachulia"};
+	public static final String HASH_TAG = " #NBA Vote";
 	
-	public static ArrayList<String> get2016EasternConferenceTweets(){
+	public static ArrayList<String> get2017EasternConferenceTweets(){
 		ArrayList<String> tweets = new ArrayList<String>();
 		for (String player: EASTERN_CONFERENCE_PLAYERS){
-			tweets.add(player + " #NBAVote");
+			tweets.add(player + HASH_TAG);
 		}
 		return tweets;
 	}
 	
-	public static ArrayList<String> get2016WesternConferenceTweets(){
+	public static ArrayList<String> get2017WesternConferenceTweets(){
 		ArrayList<String> tweets = new ArrayList<String>();
 		for (String player: WESTERN_CONFERENCE_PLAYERS){
-			tweets.add(player + " #NBAVote");
+			tweets.add(player + HASH_TAG);
 		}
 		return tweets;
 	}
 	
-	public static ArrayList<String> getNBATweets(){
-		ArrayList<String> easternTweets = get2016EasternConferenceTweets();
-		easternTweets.addAll(get2016WesternConferenceTweets());
+	public static ArrayList<String> get2017NBATweets(){
+		ArrayList<String> easternTweets = get2017EasternConferenceTweets();
+		easternTweets.addAll(get2017WesternConferenceTweets());
 		return easternTweets;
 	}
 	
 	public static void sendNBATweets(String twitterHandle) {
-		for (String tweet: getNBATweets()){
+		for (String tweet: get2017NBATweets()){
 			Twitter4JUtility.publishTweet(tweet, twitterHandle);
 		}
 	}
