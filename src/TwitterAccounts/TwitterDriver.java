200570b5fa3f64bd77856ebfbe731f64482efb20
Kristian Sooklal <ksooklal@terpmail.umd.edu>
2018-01-13 13:56:57 -0500

    updated sample api config.txt

--- src/TwitterAccounts/TwitterDriver.java
+++ src/TwitterAccounts/TwitterDriver.java
@@ -1,7 +1,7 @@
 package TwitterAccounts;
 
 public class TwitterDriver {
-	public static void main (String [] args){
+	public static void main(String[] args) {
 		KristianSooklalTwitterAccount.sendNBATweets();
 		WizardsBestFanTwitterAccount.sendNBATweets();
 		KSOOonYFinanceTwitterAccount.sendNBATweets();
--- src/nba/NBAAllStarTwitterVotingUtility.java
+++ src/nba/NBAAllStarTwitterVotingUtility.java
@@ -1,41 +1,40 @@
 package nba;
 
 import java.util.ArrayList;
-
 import Twitter4J.Twitter4JUtility;
 
 public final class NBAAllStarTwitterVotingUtility {	
-	public static final String [] EASTERN_CONFERENCE_PLAYERS = new String [] 
+	private static final String [] EASTERN_CONFERENCE_PLAYERS = new String []
 			{"John Wall", "Bradley Beal", "Marcin Gortat", "Otto Porter", "Markieff Morris", "Kelly Oubre",
-			 "Jason Smith", "Tomas Satoransky", "Jodie Meeks"};
-	public static final String [] WESTERN_CONFERENCE_PLAYERS = new String []
-			{"Trevor Ariza"};
-	public static final String HASH_TAG_2017 = " #NBAVote";
+			 "Jason Smith", "Tomas Satoransky"};
+	private static final String [] WESTERN_CONFERENCE_PLAYERS = new String []
+			{"Trevor Ariza", "Jared Dudley"};
+	private static final String HASH_TAG_2018 = " #NBAVote";
 	
-	public static ArrayList<String> get2017EasternConferenceTweets(){
-		ArrayList<String> tweets = new ArrayList<String>();
+	private static ArrayList<String> get2018EasternConferenceTweets(){
+		ArrayList<String> tweets = new ArrayList<>();
 		for (String player: EASTERN_CONFERENCE_PLAYERS){
-			tweets.add(player + HASH_TAG_2017);
+			tweets.add(player + HASH_TAG_2018);
 		}
 		return tweets;
 	}
 	
-	public static ArrayList<String> get2017WesternConferenceTweets(){
-		ArrayList<String> tweets = new ArrayList<String>();
+	private static ArrayList<String> get2018WesternConferenceTweets(){
+		ArrayList<String> tweets = new ArrayList<>();
 		for (String player: WESTERN_CONFERENCE_PLAYERS){
-			tweets.add(player + HASH_TAG_2017);
+			tweets.add(player + HASH_TAG_2018);
 		}
 		return tweets;
 	}
 	
-	public static ArrayList<String> get2017NBATweets(){
-		ArrayList<String> easternTweets = get2017EasternConferenceTweets();
-		easternTweets.addAll(get2017WesternConferenceTweets());
+	public static ArrayList<String> get2018NBATweets(){
+		ArrayList<String> easternTweets = get2018EasternConferenceTweets();
+		easternTweets.addAll(get2018WesternConferenceTweets());
 		return easternTweets;
 	}
 	
 	public static void sendNBATweets(String twitterHandle) {
-		for (String tweet: get2017NBATweets()){
+		for (String tweet: get2018NBATweets()){
 			Twitter4JUtility.publishTweet(tweet, twitterHandle);
 		}
 	}
